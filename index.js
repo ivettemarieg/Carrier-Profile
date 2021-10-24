39563409b2a36f7d891588dc99c4c3a6335f491a
Ivette Marie Gonzalez Gueits <84479945+ivettemarieg@users.noreply.github.com>
2021-10-23 23:35:02 -0500

    Added functionality to Links and Menu Button

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
+//Menu Button
+const menuBtn = document.querySelector('.menu-btn')
+const navLinks = document.querySelector('.nav-links')
+const body = document.querySelector('body')
 
-// Dark Mode
-$(".inner-switch").on("click",function(){
-    if($("body").hasClass("dark")){
-        $("body").removeClass("dark")
-        $(".inner-switch").text("OFF")
+menuBtn.addEventListener("click", function(){
+    if(navLinks.classList.contains('show-links')){
+        navLinks.classList.remove('show-links')
+        body.style.overflow = "auto"
     }
     else{
-        $("body").addClass("dark")
-        $(".inner-switch").text("ON")
+        navLinks.classList.add('show-links')
+        body.style.overflow = "hidden"
     }
 })
+
+
+navLinks.addEventListener('click', function(){
+    if(navLinks.classList.contains('show-links')){
+        navLinks.classList.remove('show-links')
+        body.style.overflow = "auto"
+    }
+})
+
+// Scrolling Links (Needs fixing)
+
+// navLinks.forEach(function(link){
+//     link.addEventListener("click", function(){
+//         e.preventDefault()
+        
+//         //To direct user to specified section
+//         const sectionId = e.currentTarget.getAttribute('href').slice(1)
+//         const element = document.getElementById(sectionId)
+//         let sectionStart = element.offsetTop
+//         window.scrollTo({
+//             left:0,
+//             top: sectionStart
+//         })
+//     })
+// })
+
+
+
+// Dark Mode
+// $(".inner-switch").on("click",function(){
+//     if($("body").hasClass("dark")){
+//         $("body").removeClass("dark")
+//         $(".inner-switch").text("OFF")
+//     }
+//     else{
+//         $("body").addClass("dark")
+//         $(".inner-switch").text("ON")
+//     }
+// })
